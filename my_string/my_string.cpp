f43e74a805fdf837dd43f8d6256f14ae8e0c457b
zzlev <gpalektzja@gmail.com>
2021-03-08 23:33:52 +0900

    Add my_string translation unit

---
+++ my_string/my_string.cpp
@@ -0,0 +1,40 @@
+#include "my_string.hpp"
+#include <algorithm>
+
+void MyString::appendStr(char *str) {   
+    char *newStr = new char[getStrLen() + strlen(str) + 1];
+    for (int i = 0; i < getStrLen(); ++i) {
+        newStr[i] = m_str[i];
+    }
+
+    for (int i = 0; i < strlen(str); ++i) {
+        newStr[getStrLen() + i] = m_str[i];
+    }
+    
+    m_strLen = getStrLen() + strlen(str);
+    delete[] m_str;
+    m_str = newStr;
+}
+
+bool MyString::isEqual(char *str) {
+    if (getStrLen() != strlen(str))
+        return false;
+    
+    for (int i = 0; i < getStrLen(); ++i) {
+        if (m_str[i] != str[i])
+            return false;
+    }
+
+    return true;
+}
+
+bool MyString::isGreater(char *str) {
+    int minLen = std::min(getStrLen(), strlen(str));
+    
+    for (int i = 0; i < minLen; ++i) {
+        if (m_str[i] > str[i])
+            return true;
+    }
+
+    return false;
+}
---
+++ my_string/my_string.hpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string.h>
+
+
+class MyString {
+    public:
+        MyString(char ch): m_strLen(1) {
+            std::cout << "MyString(char ch)" << std::endl;
+            m_str = new char[2];
+            m_str[0] = ch;
+        }
+
+        MyString(char *str): m_strLen(strlen(str)) {
+            std::cout << "MyString(char *str)" << std::endl;
+            m_str = new char[strlen(str) + 1];
+            strcpy(m_str, str);
+        }
+
+        int getStrLen() { return m_strLen; }
+
+        void appendStr(char *str);
+        // 문자열 내 포함되어 있는 문자열?
+        bool isEqual(char *str);
+        bool isGreater(char *str);
+
+        ~MyString() {
+            delete[] m_str;
+        }
+
+    private:
+        char *m_str;
+        int m_strLen;
+};
